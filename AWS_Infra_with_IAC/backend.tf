07e5bf5101923b41e2930bde0d79691b17ce0587
Sumidha sethi <38558749+sumidhasethi@users.noreply.github.com>
2025-05-02 00:11:07 +0530

    Update backend.tf

--- AWS_Infra_with_IAC/backend.tf
+++ AWS_Infra_with_IAC/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "aws-infrabucket101"       # Replace with your actual bucket
+    key            = "aws-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"             # Replace with your lock table
+    encrypt        = true
+  }
+}
 
