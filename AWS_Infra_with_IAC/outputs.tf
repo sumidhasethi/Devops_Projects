7341378753f5335eeb2b653bb0227b810a9f6359
Sumidha sethi <38558749+sumidhasethi@users.noreply.github.com>
2025-05-02 18:01:47 +0530

    Update outputs.tf

--- AWS_Infra_with_IAC/outputs.tf
+++ AWS_Infra_with_IAC/outputs.tf
@@ -1,8 +1,19 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_instance_ip" {
-  value = aws_instance.public_ec2.public_ip
-}
-
+      output "vpc_id" {
+        description = "The ID of the VPC"
+        value       = aws_vpc.main.id
+      }
+      
+      output "public_subnet_id" {
+        description = "The ID of the public subnet"
+        value       = aws_subnet.public.id
+      }
+      
+      output "private_subnet_id" {
+        description = "The ID of the private subnet"
+        value       = aws_subnet.private.id
+      }
+      
+      output "public_instance_public_ip" {
+        description = "The public IP of the EC2 instance"
+        value       = aws_instance.public_ec2.public_ip
+      }
